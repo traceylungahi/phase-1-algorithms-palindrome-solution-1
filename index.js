649e197c211d412ded45670e0737215bd8bb3751
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-16 18:58:12 +0300

    commit tree 0f91784db3aac1ba9fbccdaf0fb254af5d0398f2

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
+}
+
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
 */
-
+// reverse the input string
+  // if the reversed string is the same as the input
+  //   return true
+  // else
+  //   return false
 /*
   Add written explanation of your solution here
 */
@@ -15,6 +25,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
